03f449f19f401c0687e4dd840d4f5b2d94eff7ed
mouse <alftavares1985@gmail.com>
2017-05-27 19:23:43 +0100

    created methods for the GameObject Class

--- src/org/academiadecodigo/bootcamp8/projects/snakemouse/gameobject/GameObject.java
+++ src/org/academiadecodigo/bootcamp8/projects/snakemouse/gameobject/GameObject.java
@@ -1,6 +1,8 @@
 package org.academiadecodigo.bootcamp8.projects.snakemouse.gameobject;
 
+import org.academiadecodigo.bootcamp8.projects.snakemouse.gameobject.representable.grid.Grid;
 import org.academiadecodigo.bootcamp8.projects.snakemouse.gameobject.representable.grid.position.AbstractGridPosition;
+import org.academiadecodigo.bootcamp8.projects.snakemouse.gameobject.representable.grid.position.GridPosition;
 
 import java.awt.*;
 
@@ -9,8 +11,20 @@ import java.awt.*;
  */
 public abstract class GameObject {
 
-    public AbstractGridPosition getPosition(){
-        return null;
+    private GridPosition position;
+    private Grid grid;
+    private GameObjectType objectType;
+
+    public GameObject(GridPosition position, GameObjectType objectType){
+        this.position = position;
+        this.objectType = objectType;
     }
 
+    public void setGrid(Grid grid) {
+        this.grid = grid;
+    }
+
+    public GridPosition getPosition() {
+        return position;
+    }
 }
