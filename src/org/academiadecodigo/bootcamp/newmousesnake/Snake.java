c9ca1e1a6759ebd43771920e9b727fa4e5376662
joelalmeida39 <joelalmeida39@gmail.com>
2017-05-31 19:37:08 +0100

    Class Snake

---
+++ src/org/academiadecodigo/bootcamp/newmousesnake/Snake.java
@@ -0,0 +1,19 @@
+package org.academiadecodigo.bootcamp.newmousesnake;
+
+import org.academiadecodigo.bootcamp.newmousesnake.Grid.Position.GridPosition;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+/**
+ * Created by joelalmeida on 31/05/17.
+ */
+public class Snake {
+
+    private GridPosition position;
+
+    private Picture picLeft;
+    private Picture picRight;
+    private Picture picUp;
+    
+
+
+}
